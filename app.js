4ebc2eca6c5cb5c9d5801596ddd6da45c5f4d41b
Mooncode29 <irene.ravelonarivo@gmail.com>
2016-10-07 12:25:22 +0200

    exo ok, bonus inclus

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
 	//votre code
+	var counter = 0;
 
+	$('.cake-is-a-lie').click(function(){
+		counter++;
+		$('.counter').text(counter);
+
+		if (counter % 1 === 0) {
+		alert("vous ferez mieux de courir!");
+		var arr = ["vous ferez mieux de courir", "vous ferez mieux de lire", "vous ferez mieux developper", "blabalabla"]; 
+		var aleatoire = arr[Math.floor(Math.random()*arr.length)];
+		alert(aleatoire);
+		}
+
+	
+
+	
+	});
 
 });
